61647b0dde092bfe0533d9b75aba09b7f712dea9
Arnav <arnavmodgil08@gmail.com>
2026-07-03 01:53:14 +0530

    New code

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello World");
+//     }
+// }
+
+// Input from User code :
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+        System.out.println(name);
+
     }
-    
 }
+
+
 
