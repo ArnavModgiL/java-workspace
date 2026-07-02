55c40ffb3ba4dced920056d5d77d31a893e4fa1e
Arnav <arnavmodgil08@gmail.com>
2026-07-03 02:10:44 +0530

    Add try

--- Main.java
+++ Main.java
@@ -8,11 +8,11 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Name: ");
-        String name = sc.nextLine();
-        System.out.println(name);
-        sc.close();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter Name: ");
+            String name = sc.nextLine();
+            System.out.println(name);
+        }
 
     }
 }
