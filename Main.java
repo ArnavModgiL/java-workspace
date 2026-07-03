a6782b36241e8bae86007d6422bdc3e365bd4821
Arnav <arnavmodgil08@gmail.com>
2026-07-03 13:37:30 +0530

    Adding Two Number

--- Main.java
+++ Main.java
@@ -22,22 +22,19 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Scanner object create kiya
-        Scanner sc = new Scanner(System.in);
-
         // User first number lena
-        System.out.print("Enter First Number: ");
-        int a = sc.nextInt();
-
-        // User se second number lena
-        System.out.print("Enter Second Number: ");
-        int b = sc.nextInt();
-
-        int sum = a + b;
-        System.out.println("Sum = " + sum);
-
-        // Scanner close karna
-        sc.close();
+        try ( // Scanner object create kiya
+                Scanner sc = new Scanner(System.in)) {
+            // User first number lena
+            System.out.print("Enter First Number: ");
+            int a = sc.nextInt();
+            // User se second number lena
+            System.out.print("Enter Second Number: ");
+            int b = sc.nextInt();
+            int sum = a + b;
+            System.out.println("Sum = " + sum);
+            // Scanner close karna
+        }
 
 
     }
