de5381d7e836dfaa1c4cffc61da067b818929c15
Arnav <arnavmodgil08@gmail.com>
2026-07-03 13:33:53 +0530

    Adding Two Number

--- Main.java
+++ Main.java
@@ -5,17 +5,40 @@
 // }
 
 // Input from User code :
+
+// import java.util.Scanner;
+// public class Main {
+//     public static void main(String[] args) {
+//         try (Scanner sc = new Scanner(System.in)) {
+//             System.out.print("Enter Name: ");
+//             String name = sc.nextLine();
+//             System.out.println(name);
+//         }
+
+//     }
+// }
+
+// Adding Two Numbers :
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter Name: ");
-            String name = sc.nextLine();
-            System.out.println(name);
-        }
+        // Scanner object create kiya
+        Scanner sc = new Scanner(System.in);
+
+        // User first number lena
+        System.out.print("Enter First Number: ");
+        int a = sc.nextInt();
+
+        // User se second number lena
+        System.out.print("Enter Second Number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+
+        // Scanner close karna
+        sc.close();
+
 
     }
 }
-
-
-
