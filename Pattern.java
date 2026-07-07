6b86466339c0435a566c362048d9ee4f1715f1c0
Arnav <arnavmodgil08@gmail.com>
2026-07-07 14:27:03 +0530

    Adding Inverted Triangle Question

---
+++ Pattern.java
@@ -0,0 +1,52 @@
+// SQUARE PATTERN QUESTION : --->
+
+// import java.util.Scanner;
+// public class Pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i = 1; i<= n ; i++){
+//             for(int j = 1; j <= n ; j++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+//     }
+// }
+
+
+// RIGHT TRIANGLE CODE : --->
+
+// import java.util.Scanner;
+// public class Pattern {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i = 1; i<= n; i++){
+//             for(int j = 1; j <= i; j++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Inverted Triangle Question:
+
+import java.util.*; 
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = n ; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
