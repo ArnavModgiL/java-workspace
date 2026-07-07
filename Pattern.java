9d3bfcc52365ac5b4d7e5c6ef55d8dc932077138
Arnav <arnavmodgil08@gmail.com>
2026-07-07 14:58:45 +0530

    Adding Number Triangle

--- Pattern.java
+++ Pattern.java
@@ -36,17 +36,36 @@
 
 // Inverted Triangle Question:
 
-import java.util.*; 
+// import java.util.*; 
+// public class Pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for(int i = n ; i >= 1; i--){
+//             for(int j = 1; j <= i; j++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+//     }
+// }
+
+// Number Triangle Question : 
+
+import java.util.*;
 public class Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        for(int i = n ; i >= 1; i--){
-            for(int j = 1; j <= i; j++){
-                System.out.print("* ");
+        for(int i = 1; i<= n; i++){
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j+ " ");
             }
             System.out.println();
         }
-        sc.close();
     }
+
 }
+
