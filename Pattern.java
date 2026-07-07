9dffe449cb3dbb54b03309ed0cf0c6024a8a3053
Arnav <arnavmodgil08@gmail.com>
2026-07-07 15:24:50 +0530

    Adding Floyd's Triangle

--- Pattern.java
+++ Pattern.java
@@ -53,18 +53,37 @@
 
 // Number Triangle Question :  
 
-import java.util.*;
+// import java.util.*;
+// public class Pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+//         for(int i = 1; i<= n; i++){
+//             for(int j = 1; j <= i; j++) {
+//                 System.out.print(j+ " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Floyid's Triangle code Question :
+import java.util.Scanner;
 public class Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        for(int i = 1; i<= n; i++){
-            for(int j = 1; j <= i; j++) {
-                System.out.print(j+ " ");
+
+        int num = sc.nextInt();
+
+        for(int i = 1; i<= n ; i++){
+            for(int j = 1; j <= i ; j++){
+                System.out.print(num + " ");
+                num++;
             }
             System.out.println();
         }
+
     }
 }
-
