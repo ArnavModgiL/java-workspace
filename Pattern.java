3d34e7c7c71604136b973925c19e2be6ce5ad4df
Arnav <arnavmodgil08@gmail.com>
2026-07-07 15:45:15 +0530

    Adding Pyramid Question Code

--- Pattern.java
+++ Pattern.java
@@ -70,18 +70,41 @@
 
 // Floyid's Triangle code Question :
 
+// import java.util.Scanner;
+// public class Pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         int num = sc.nextInt();
+
+//         for(int i = 1; i<= n ; i++){
+//             for(int j = 1; j <= i ; j++){
+//                 System.out.print(num + " ");
+//                 num++;
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+// Pyramid Question Code :
+
 import java.util.Scanner;
-public class Pattern {
+public class Pattern{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
 
-        int num = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
 
-        for(int i = 1; i<= n ; i++){
-            for(int j = 1; j <= i ; j++){
-                System.out.print(num + " ");
-                num++;
+            for(int j = 1; j<= (2 * i - 1); j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
