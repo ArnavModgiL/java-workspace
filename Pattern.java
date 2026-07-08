17e1cb9006f8fbf2535889754bacf6e4bbbb1a67
Arnav <arnavmodgil08@gmail.com>
2026-07-09 00:39:55 +0530

    Adding Palindromic Pattern

--- Pattern.java
+++ Pattern.java
@@ -346,25 +346,75 @@
 
 // Hollow Rhombus Question : --->
 
-import java.util.*;
+// import java.util.*;
+// public class Pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i = 1; i<= n; i++){
+//             for(int j = 1; j<= n-i; j++){
+//                 System.out.print(" ");
+//             }
+
+//             for(int j = 1; j<= n; j++){
+//                 if(i == 1 || j == 1 || j== n|| i == n) {
+//                     System.out.print("*");
+//                 } else {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// Number Pyramid Question :---->
+
+// import java.util.Scanner;
+// public class Pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i = 1 ; i<= n; i++){
+//             for(int j = 1; j<= n- i;j++){
+//                 System.out.print(" ");
+//             }
+
+//             for(int j = 1; j<= i; j++){
+//                 System.out.print(i + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// Palindromic Pattern : --->
+
+import java.util.Scanner;
 public class Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         for(int i = 1; i<= n; i++){
-            for(int j = 1; j<= n-i; j++){
+            for(int j = 1 ; j<= n-i; j++){
                 System.out.print(" ");
             }
 
-            for(int j = 1; j<= n; j++){
-                if(i == 1 || j == 1 || j== n|| i == n) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+            for(int j = i; j >= 1; j--){
+                System.out.print(j);
+            }
+            
+            for(int j = 2; j <= i ; j++){
+                System.out.print(j);
             }
             System.out.println();
         }
     }
 }
+
+
