3f2259a19702db7868334de32ca2989ca160327f
Arnav <arnavmodgil08@gmail.com>
2026-07-08 20:36:13 +0530

    0-1 Triangle Question!

--- Pattern.java
+++ Pattern.java
@@ -252,17 +252,39 @@
 
 // Solid Rectangle : 
 
-import java.util.*;
+// import java.util.*;
+// public class Pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int row = sc.nextInt();
+//         int col = sc.nextInt();
+
+//         for(int i = 1; i<= row; i++) {
+//             for(int j = 1; j <= col; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// 0 - 1 Triangle Question Code : 
+
+
+import java.util.Scanner;
 public class Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        int row = sc.nextInt();
-        int col = sc.nextInt();
-
-        for(int i = 1; i<= row; i++) {
-            for(int j = 1; j <= col; j++) {
-                System.out.print("*");
+        for(int i = 1; i <= n ; i++){
+            for(int j = 1; j <= i; j++){
+                if((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
             }
             System.out.println();
         }
