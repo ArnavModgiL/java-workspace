aba15f8f0bdefa47418bf2b8db183d8b63501871
Arnav <arnavmodgil08@gmail.com>
2026-07-09 00:54:30 +0530

    Adding Half Pyramid Question!

--- Pattern.java
+++ Pattern.java
@@ -394,6 +394,31 @@
 
 // Palindromic Pattern : --->
 
+// import java.util.Scanner;
+// public class Pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i = 1; i<= n; i++){
+//             for(int j = 1 ; j<= n-i; j++){
+//                 System.out.print(" ");
+//             }
+
+//             for(int j = i; j >= 1; j--){
+//                 System.out.print(j);
+//             }
+            
+//             for(int j = 2; j <= i ; j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Half Pyramid (Number) :--->
+
 import java.util.Scanner;
 public class Pattern {
     public static void main(String[] args) {
@@ -401,20 +426,10 @@ public class Pattern {
         int n = sc.nextInt();
 
         for(int i = 1; i<= n; i++){
-            for(int j = 1 ; j<= n-i; j++){
-                System.out.print(" ");
-            }
-
-            for(int j = i; j >= 1; j--){
-                System.out.print(j);
-            }
-            
-            for(int j = 2; j <= i ; j++){
-                System.out.print(j);
+            for(int j = 1; j<= i ; j++){
+                System.out.print(i + " ");
             }
             System.out.println();
         }
     }
 }
-
-
