ce3a92a90b8814636287013baa4b3eec02864de7
Arnav <arnavmodgil08@gmail.com>
2026-07-08 10:46:01 +0530

    Adding Hollow Square Question

--- Pattern.java
+++ Pattern.java
@@ -114,31 +114,54 @@
 
 // Diamond Pattern Question : 
 
+// import java.util.*;
+// public class Pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i = 1; i<= n; i++){
+//             for(int j = 1 ; j<= n - i; j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 1 ; j<= (2 * i - 1); j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+ 
+//         for(int i = n - 1; i >= 1; i--){
+//             for(int j = 1; j<= n - i; j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 1; j <= (2 * i - 1); j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// Hollow Square Code : 
+
 import java.util.*;
 public class Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i = 1; i<= n; i++){
-            for(int j = 1 ; j<= n - i; j++){
-                System.out.print(" ");
-            }
-            for(int j = 1 ; j<= (2 * i - 1); j++) {
-                System.out.print("*");
+        for(int i = 1; i<=n ; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == 1 || i == n || j == 1 || j == n){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
 
- 
-        for(int i = n - 1; i >= 1; i--){
-            for(int j = 1; j<= n - i; j++){
-                System.out.print(" ");
-            }
-            for(int j = 1; j <= (2 * i - 1); j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
     }
 }
