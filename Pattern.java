85e6c14f9f31f18b1d879c293c1142ac3cfdf9c0
Arnav <arnavmodgil08@gmail.com>
2026-07-08 11:19:12 +0530

    Adding Reverse Number Triangle

--- Pattern.java
+++ Pattern.java
@@ -168,23 +168,40 @@
 
 // Hollow Rectangle Question : 
 
-import java.util.Scanner;
+// import java.util.Scanner;
+// public class Pattern {
+//     public static void main(String[] args) {
+        
+//         Scanner sc = new Scanner(System.in);
+//         // Row ki input li hai :-
+//         int row = sc.nextInt();
+//         // Col ki input li hai :-
+//         int col = sc.nextInt();
+
+//         for(int i = 1; i<= row ; i++){
+//             for(int j = 1; j<= col; j++){
+//                 if(i == 1 || i == row || j == 1 || j == col){
+//                     System.out.print("*");
+//                 } else {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Reverse Number Triangle :
+
+import java.util.*;
 public class Pattern {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
-        // Row ki input li hai :-
-        int row = sc.nextInt();
-        // Col ki input li hai :-
-        int col = sc.nextInt();
+        int n = sc.nextInt();
 
-        for(int i = 1; i<= row ; i++){
-            for(int j = 1; j<= col; j++){
-                if(i == 1 || i == row || j == 1 || j == col){
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+        for(int i = 1; i<= n ; i++){
+            for(int j = n ; j >= n - i + 1; j--){
+                System.out.print(j + " ");
             }
             System.out.println();
         }
