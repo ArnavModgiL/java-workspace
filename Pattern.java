b5c94b8d693ff94ec31eb3ae33861e61ad4aa732
Arnav <arnavmodgil08@gmail.com>
2026-07-09 16:26:23 +0530

    Adding Hollow Pyramid Question!

--- Pattern.java
+++ Pattern.java
@@ -419,15 +419,42 @@
 
 // Half Pyramid (Number) :--->
 
-import java.util.Scanner;
+// import java.util.Scanner;
+// public class Pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i = 1; i<= n; i++){
+//             for(int j = 1; j<= i ; j++){
+//                 System.out.print(i + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// Hollow Pyramid Question : 
+
+import java.util.*;
 public class Pattern {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i = 1; i<= n; i++){
-            for(int j = 1; j<= i ; j++){
-                System.out.print(i + " ");
+        for(int i = 1; i<= n; i++) {
+            for(int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= (2 * i - 1); j++) {
+                if(i == n || j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
