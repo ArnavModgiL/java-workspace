9bad48c13b7049b4013ac101e7125632bdc3e2de
Arnav <arnavmodgil08@gmail.com>
2026-07-10 13:15:08 +0530

    Adding X Pattern!.

--- Pattern.java
+++ Pattern.java
@@ -530,30 +530,52 @@
 
 // Sandglass Pattern Question : --->
 
+// import java.util.Scanner;
+// public class Pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i = n; i >= 1; i--) {
+//             for(int j = 1; j <= n - i; j++) {
+//                 System.out.print(" ");
+//             }
+
+//             for(int j = 1; j<= (2 * i - 1); j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+//         for(int i = 1; i<= n; i++) {
+//             for(int j = 1 ; j <= n - i; j++) {
+//                 System.out.print(" ");
+//             }
+
+//             for(int j = 1 ; j <= (2 * i -1); j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// X Pattern Question : --->
+
 import java.util.Scanner;
 public class Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i = n; i >= 1; i--) {
-            for(int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-
-            for(int j = 1; j<= (2 *i - 1); j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        
-        for(int i = 1; i<= n; i++) {
-            for(int j = 1 ; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-
-            for(int j = 1 ; j <= (2 * i -1); j++) {
-                System.out.print("*");
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j<= n; j++) {
+                if(j == i || j == n - i + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
