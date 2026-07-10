dec0b9a1ab5e5260bdd50e550277e78a7144fbc6
Arnav <arnavmodgil08@gmail.com>
2026-07-10 15:12:43 +0530

    Adding Prime Number Question!

--- Question.java
+++ Question.java
@@ -203,31 +203,58 @@
 
 // Maximum of Three Number : 
 
-import java.util.*;
+// import java.util.*;
+// public class Question {
+//     public static int maximum(int a, int b, int c) {
+//         if(a >= b && a >= c){
+//             return a;
+//         } else if (b >= a && b >= c) {
+//             return b;
+//         } else {
+//             return c;
+//         }
+//     }
+//         public static void main(String[] args) {
+//             Scanner sc = new Scanner(System.in);
+
+//             System.out.print("Enter First Number : ");
+//             int a = sc.nextInt();
+
+//             System.out.print("Enter Second Number : ");
+//             int b = sc.nextInt();
+
+//             System.out.print("Enter Third Number : ");
+//             int c = sc.nextInt();
+
+//             int ans = maximum(a,b,c);
+//             System.out.println("Largest Number = " + ans);
+//         }
+// }
+
+// Prime Number Question : 
+
+import java.util.Scanner;
 public class Question {
-    public static int maximum(int a, int b, int c) {
-        if(a >= b && a >= c){
-            return a;
-        } else if (b >= a && b >= c) {
-            return b;
+    public static Boolean isPrime(int n) {
+        if(n <= 1){
+            return false;
+        }
+        for(int i = 2; i<= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        if(isPrime(n)) {
+            System.out.println("Prime Number!");
         } else {
-            return c;
+            System.out.println("Not Prime!");
         }
     }
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-
-            System.out.print("Enter First Number : ");
-            int a = sc.nextInt();
-
-            System.out.print("Enter Second Number : ");
-            int b = sc.nextInt();
-
-            System.out.print("Enter Third Number : ");
-            int c = sc.nextInt();
-
-            int ans = maximum(a,b,c);
-            System.out.println("Largest Number = " + ans);
-        }
-
 }
