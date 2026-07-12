37ff069e17c5feb92bb25e13dcaa67097e0b80f6
Arnav <arnavmodgil08@gmail.com>
2026-07-12 17:11:49 +0530

    Minimum Element!

--- Array.java
+++ Array.java
@@ -126,25 +126,50 @@
 
 // Maximum Element! 
 
+// import java.util.Scanner;
+// public class Array{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+
+//         for(int i = 0 ; i< n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         int max  = arr[0];
+//         for(int i = 0 ; i< arr.length; i++) {
+//             if(arr[i] > max) {
+//                 max = arr[i];
+//             }
+//         }
+//         System.out.println("Maximum = " + max);
+
+//     }
+// }
+
+// Minimum Element! 
+
 import java.util.Scanner;
-public class Array{
+public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         int[] arr = new int[n];
 
-        for(int i = 0 ; i< n; i++) {
+        for(int i = 0 ; i<n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        int max  = arr[0];
-        for(int i = 0 ; i< arr.length; i++) {
-            if(arr[i] > max) {
-                max = arr[i];
+        int min = arr[0];
+
+        for(int i = 0; i< arr.length;i++) {
+            if(arr[i] < min) {
+                min = arr[i];
             }
         }
-        System.out.println("Maximum = " + max);
-
+        System.out.println("Minimum = " + min);
     }
 }
