dad96e1bef25d8da670a6c4a76beb36b565e2022
Arnav <arnavmodgil08@gmail.com>
2026-07-12 16:50:40 +0530

    Print Elements!

--- Array.java
+++ Array.java
@@ -43,39 +43,61 @@
 
 // Find x in 2D Matrix Question : 
 
+// import java.util.Scanner;
+// public class Array {
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter Number of rows : ");
+//         int n = sc.nextInt();
+//         System.out.println("Enter the Number of Cols : ");
+//         int m = sc.nextInt();
+
+//         int[][] arr = new int[n][m];
+
+//         for(int i = 0; i < n ; i++){
+//             for(int j = 0; j < m; j++) {
+//                 arr[i][j] = sc.nextInt();
+//             }
+//         }
+//         System.out.println("Enter element to search!");
+//         int x = sc.nextInt();
+
+//         Boolean found = false;
+
+//         for(int i = 0; i < n ; i++) {
+//             for(int j = 0; j < m; j++) {
+//                 if(arr[i][j] == x) {
+//                     System.out.println("Element Found at rows = " + i + ", column = " + j);
+//                     found = true;
+//                 }
+//             }
+//         }
+
+//         if(! found) {
+//             System.out.println("Element not found!");
+//         }
+
+//     }
+// }
+
+
+// Print All element! 
+
 import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number of rows : ");
+
         int n = sc.nextInt();
-        System.out.println("Enter the Number of Cols : ");
-        int m = sc.nextInt();
+        int[] arr = new int[n];
 
-        int[][] arr = new int[n][m];
-
-        for(int i = 0; i < n ; i++){
-            for(int j = 0; j < m; j++) {
-                arr[i][j] = sc.nextInt();
-            }
-        }
-        System.out.println("Enter element to search!");
-        int x = sc.nextInt();
-
-        Boolean found = false;
-
-        for(int i = 0; i < n ; i++) {
-            for(int j = 0; j < m; j++) {
-                if(arr[i][j] == x) {
-                    System.out.println("Element Found at rows = " + i + ", column = " + j);
-                    found = true;
-                }
-            }
+        for(int i = 0 ; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        if(! found) {
-            System.out.println("Element not found!");
+        for(int i = 0 ; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
 
     }
