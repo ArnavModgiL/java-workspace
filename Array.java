22a6385026015d851d8fde7fbee1c29ed8b472bc
Arnav <arnavmodgil08@gmail.com>
2026-07-13 13:13:50 +0530

    Copy Array!

--- Array.java
+++ Array.java
@@ -230,21 +230,46 @@
 
 // Reverse an Array! 
 
+// import java.util.Scanner;
+// public class Array {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+
+//         for(int i = 0; i< n; i++){
+//             arr[i] = sc.nextInt();
+//         }
+
+
+//         for(int i = arr.length - 1; i >= 0; i--){
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+
+// }
+
+// Copy one Array intp Another !
+
 import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
         int[] arr = new int[n];
 
-        for(int i = 0; i< n; i++){
+        for(int i=0; i< n; i++){
             arr[i] = sc.nextInt();
         }
 
+        int[] copy = new int[arr.length];
+        for(int i = 0; i< n; i++) {
+            copy[i] = arr[i];
+        }
 
-        for(int i = arr.length - 1; i >= 0; i--){
-            System.out.print(arr[i] + " ");
+        for(int i = 0; i< n; i++) {
+            System.out.print(copy[i] + " ");
         }
     }
-
 }
