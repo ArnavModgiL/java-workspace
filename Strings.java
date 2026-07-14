9e376e224d94891d7400003c9f2b940b62de745e
Arnav <arnavmodgil08@gmail.com>
2026-07-14 13:48:10 +0530

    Reverse String!

---
+++ Strings.java
@@ -0,0 +1,29 @@
+// Reverse String! 
+
+import java.util.*;
+public class Strings {
+    public static String reverse(String str) {
+        char[] arr = str.toCharArray(); // character array bnta hai yee !
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(arr);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the word! ");
+        String str = sc.nextLine();
+        System.out.println(reverse(str));
+
+    }
+}
