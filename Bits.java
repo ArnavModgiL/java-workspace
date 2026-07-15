802a3d6319c08bd5743a1a047438ac95e3513de9
Arnav <arnavmodgil08@gmail.com>
2026-07-15 14:43:29 +0530

    SET BIT QUESTION!

--- Bits.java
+++ Bits.java
@@ -18,22 +18,40 @@
 // }
 
 
-// GIT BIT QUESTION (LOGIC!):
+// GET BIT QUESTION (LOGIC!):
+
+// import java.util.Scanner;
+// public class Bits {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter Number : ");
+//         int nums = sc.nextInt();
+//         System.out.print("Enter Position : ");
+//         int position = sc.nextInt();
+
+//         int bitMask = 1 << position;
+//         if((nums & bitMask) == 0) {
+//             System.out.println("Bit is 0.");
+//         } else {
+//             System.out.println("Bit is 1.");
+//         }
+//     }
+// }
+
+
+// SET BIT QUESTION (LOGIC!):
 
 import java.util.Scanner;
 public class Bits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Number : ");
-        int nums = sc.nextInt();
+        System.out.print("Enter the Number : ");
+        int number = sc.nextInt();
         System.out.print("Enter Position : ");
         int position = sc.nextInt();
-
         int bitMask = 1 << position;
-        if((nums & bitMask) == 0) {
-            System.out.println("Bit is 0.");
-        } else {
-            System.out.println("Bit is 1.");
-        }
+
+        int result = number | bitMask;
+        System.out.println(result);
     }
 }
