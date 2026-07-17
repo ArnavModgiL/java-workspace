62e9f70e2287d42d84b4df3d597f400abd866dea
Arnav <arnavmodgil08@gmail.com>
2026-07-17 15:45:54 +0530

    Insertion Sorting Question !

--- Sorting.java
+++ Sorting.java
@@ -48,31 +48,64 @@ Quick Sort.
 
 // Selection Sort Question!...
 
+// import java.util.Scanner;
+// public class Sorting {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter Size : ");
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+
+//         for(int i = 0; i< arr.length; i++){
+//             arr[i] = sc.nextInt();
+//         }
+
+//         for(int i = 0; i<n - 1; i++){
+//             int min = i;
+//             for(int j = i + 1; j < n; j++) {
+//                 if(arr[j] < arr[min]){
+//                     min = j;
+//                 }
+//             }
+//             int temp = arr[min];
+//             arr[min] = arr[i];
+//             arr[i]  = temp;
+//         }
+//         System.out.println("Sorted Array : ");
+//         for(int i =0; i<arr.length; i++){
+//             int num = arr[i];
+//             System.out.print(num + " ");
+//         }
+//     }
+// }
+
+
+// Insertion Sorting Question :
+
 import java.util.Scanner;
 public class Sorting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Size : ");
         int n = sc.nextInt();
-        int[] arr = new int[n];
 
-        for(int i = 0; i< arr.length; i++){
+        int[] arr = new int[n];
+        for(int i = 0;  i< arr.length; i++){
             arr[i] = sc.nextInt();
         }
 
-        for(int i = 0; i<n - 1; i++){
-            int min = i;
-            for(int j = i + 1; j < n; j++) {
-                if(arr[j] < arr[min]){
-                    min = j;
-                }
+        for(int i = 1; i < n; i++) {
+            int j = i - 1;
+            int key = arr[i];
+            while(j >= 0 && key < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
             }
-            int temp = arr[min];
-            arr[min] = arr[i];
-            arr[i]  = temp;
+            arr[j + 1] = key;
         }
+
         System.out.println("Sorted Array : ");
-        for(int i =0; i<arr.length; i++){
+        for(int i = 0; i< arr.length;i++){
             int num = arr[i];
             System.out.print(num + " ");
         }
