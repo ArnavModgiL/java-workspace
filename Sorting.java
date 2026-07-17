b8e65f67dff669518588bd9c9d60873a80954358
Arnav <arnavmodgil08@gmail.com>
2026-07-17 14:25:48 +0530

    Bubble Sorting Code!

---
+++ Sorting.java
@@ -0,0 +1,46 @@
+// Sorting Data ko order mei arrange karna!.
+
+/* Here We Learn. 5 types of Sorting Algorithms :-->
+
+.Bubble Sort.
+.Selection Sort.
+.Inseration Sort.
+.Merge Sort.
+Quick Sort.
+
+*/
+
+
+
+// Bubble Sorting Algorithm Question Code :
+
+import java.util.Scanner;
+public class Sorting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter ELment: ");
+        for(int i = 0 ; i< arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i<n - 1; i++){
+            for(int j = 0; j < n - 1; j++){
+                if(arr[j] > arr[ j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array : ");
+
+        for(int i = 0 ; i< arr.length; i++) {
+            int num = arr[i];
+            System.out.print(num + " ");
+        }
+    }
+}
