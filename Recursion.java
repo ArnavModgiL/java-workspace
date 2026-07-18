75e850c10ed8200c1792a7ca9d0d344c303b77f4
Arnav <arnavmodgil08@gmail.com>
2026-07-18 16:39:00 +0530

    Power (a^b).

--- Recursion.java
+++ Recursion.java
@@ -62,20 +62,44 @@
 
 // Factorial Question !
 
+// import java.util.Scanner;
+// public class Recursion {
+//     public static int fact(int n){
+//         if(n == 0 || n == 1){
+//             return 1;
+//         }
+//         return n * fact(n - 1);
+//     }
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter N: ");
+//         int n = sc.nextInt();
+//         int result = fact(n);
+//         System.out.print("Factorial = " + result);
+//     }
+// }
+
+// Power (a^b)!
+
 import java.util.Scanner;
 public class Recursion {
-    public static int fact(int n){
-        if(n == 0 || n == 1){
+    public static int power(int a, int b){
+        if(b == 0){
             return 1;
         }
-        return n * fact(n - 1);
+        return a * power(a,b-1);
     }
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter N: ");
-        int n = sc.nextInt();
-        int result = fact(n);
-        System.out.print("Factorial = " + result);
+        System.out.print("Enter A: ");
+        int a = sc.nextInt();
+        System.out.print("Enter B: ");
+        int b = sc.nextInt();
+
+        int ans = power(a,b);
+        System.out.print("Answer = "+ ans);
     }
 }
+
