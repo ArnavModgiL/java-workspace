fcbe1ae48c0edcc655a83cb85a45c286ff929407
Arnav <arnavmodgil08@gmail.com>
2026-07-18 15:03:37 +0530

    print N to 1!"

--- Recursion.java
+++ Recursion.java
@@ -4,20 +4,39 @@
 
 // Print 1 to N :
 
+// import java.util.Scanner;
+// public class Recursion {
+//     public static void printN(int n) {
+//         if(n == 0) {
+//             return;
+//         } printN(n - 1);
+//         System.out.print(n + " ");
+//     }
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter N: ");
+//         int n = sc.nextInt();
+//         printN(n);
+//     } 
+// }
+
+// Print N to 1 :
+
 import java.util.Scanner;
 public class Recursion {
-    public static void printN(int n) {
-        if(n == 0) {
+    public static void printN(int n){
+        if(n == 0){
             return;
-        } printN(n - 1);
+        }
         System.out.print(n + " ");
+        printN(n - 1);
     }
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter N: ");
         int n = sc.nextInt();
         printN(n);
-    } 
+    }
 }
