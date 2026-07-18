c899ac43b8bf7c4a1004cb71f2c5379756989aa1
Arnav <arnavmodgil08@gmail.com>
2026-07-18 22:24:41 +0530

    Fibonacci Number!

--- Recursion.java
+++ Recursion.java
@@ -82,24 +82,43 @@
 
 // Power (a^b)!
 
+// import java.util.Scanner;
+// public class Recursion {
+//     public static int power(int a, int b){
+//         if(b == 0){
+//             return 1;
+//         }
+//         return a * power(a,b-1);
+//     }
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter A: ");
+//         int a = sc.nextInt();
+//         System.out.print("Enter B: ");
+//         int b = sc.nextInt();
+
+//         int ans = power(a,b);
+//         System.out.print("Answer = "+ ans);
+//     }
+// }
+
+
+// Fibonacci Number !
+
 import java.util.Scanner;
 public class Recursion {
-    public static int power(int a, int b){
-        if(b == 0){
-            return 1;
+    public static int fib(int n){
+        if(n <= 1){
+            return n;
         }
-        return a * power(a,b-1);
+        return fib(n - 1) + fib(n - 2);
     }
-
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter A: ");
-        int a = sc.nextInt();
-        System.out.print("Enter B: ");
-        int b = sc.nextInt();
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+        System.out.println(fib(n));
 
-        int ans = power(a,b);
-        System.out.print("Answer = "+ ans);
     }
 }
-
