50aa29fa0d5935468ec14dd75adf81e20c2d24bb
Arnav <arnavmodgil08@gmail.com>
2026-07-18 15:34:48 +0530

    Sum of N Number!

--- Recursion.java
+++ Recursion.java
@@ -24,19 +24,38 @@
 
 // Print N to 1 :
 
+// import java.util.Scanner;
+// public class Recursion {
+//     public static void printN(int n){
+//         if(n == 0){
+//             return;
+//         }
+//         System.out.print(n + " ");
+//         printN(n - 1);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter N: ");
+//         int n = sc.nextInt();
+//         printN(n);
+//     }
+// }
+
+
+// Sum of First N Number ! 
+
 import java.util.Scanner;
 public class Recursion {
-    public static void printN(int n){
-        if(n == 0){
-            return;
-        }
-        System.out.print(n + " ");
-        printN(n - 1);
+    public static int sum(int n){
+        if(n == 0)
+            return 0;
+        return n + sum(n - 1);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter N: ");
         int n = sc.nextInt();
-        printN(n);
+        System.out.println("Sum = "+ sum(n));
     }
 }
