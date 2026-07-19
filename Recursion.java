61bdac8a9ce46ef99599fa67905c6e8e71c6adcd
Arnav <arnavmodgil08@gmail.com>
2026-07-19 11:03:22 +0530

    Sum of Digits!

--- Recursion.java
+++ Recursion.java
@@ -106,19 +106,37 @@
 
 // Fibonacci Number !
 
+// import java.util.Scanner;
+// public class Recursion {
+//     public static int fib(int n){
+//         if(n <= 1){
+//             return n;
+//         }
+//         return fib(n - 1) + fib(n - 2);
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter N: ");
+//         int n = sc.nextInt();
+//         System.out.println(fib(n));
+
+//     }
+// }
+
+
+// Sum of Digits :
+
 import java.util.Scanner;
 public class Recursion {
-    public static int fib(int n){
-        if(n <= 1){
-            return n;
-        }
-        return fib(n - 1) + fib(n - 2);
+    public static int sumDigits(int n) {
+        if(n == 0)
+            return 0;
+        return n % 10 + sumDigits(n / 10);
     }
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter N: ");
-        int n = sc.nextInt();
-        System.out.println(fib(n));
-
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Number : ");
+    int n = sc.nextInt();
+    System.out.println("Sum = "+ sumDigits(n));
     }
 }
