4e2450cd3d0c73c52e1809a10b6e6cf218eeca51
Arnav <arnavmodgil08@gmail.com>
2026-07-26 13:47:23 +0530

    Print Subsequence of String!

--- Recursion.java
+++ Recursion.java
@@ -390,4 +390,25 @@
 //     }
 // }
 
-// 
+// Print all Substring of String !
+
+import java.util.Scanner;
+public class Recursion {
+    public static void subsequence(String str, int idx, String newString) {
+        if(idx == str.length()) {
+            System.out.println(newString);
+            return;
+        } char currChar = str.charAt(idx);
+
+        subsequence(str, idx + 1, newString + currChar);
+
+        subsequence(str, idx + 1, newString);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String str = sc.nextLine();
+        subsequence(str,0,"");
+    }
+}
