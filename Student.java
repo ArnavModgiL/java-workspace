da1eb5fa625b7ff03efce24d35fe001c9c496a92
Arnav <arnavmodgil08@gmail.com>
2026-08-10 11:46:46 +0530

    ADDING Q2.

--- Student.java
+++ Student.java
@@ -1,16 +1,32 @@
 // Here we solve the Question Bank Soln!
 
+// class Student {
+//     String name;
+//     int age;
+
+//     public static void main(String[] args) {
+//         Student s1 = new Student();
+
+//         s1.name = "Arnav";
+//         s1.age = 18;
+
+//         System.out.println("Name: "+ s1.name);
+//         System.out.println("Age: " + s1.age);
+//     }
+// }
+
+
 class Student {
-    String name;
-    int age;
+    String brand;
+    double price;
 
     public static void main(String[] args) {
-        Student s1 = new Student();
+        Student c1 = new Student();
 
-        s1.name = "Arnav";
-        s1.age = 18;
+        c1.brand = "Toyota";
+        c1.price = 250000;
 
-        System.out.println("Name: "+ s1.name);
-        System.out.println("Age: " + s1.age);
+        System.out.println("Brand " + c1.brand);
+        System.out.println("Price:" + c1.price);
     }
 }
